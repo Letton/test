7dcbe29c96021c6183744fcbae6318f9482edba9
Letton <admin@letton.tech>
2021-10-21 19:56:39 +0300

    Ball_Collide

--- main.py
+++ main.py
@@ -4,69 +4,86 @@ import random
 
 size = width, height = 800, 600
 black = 0, 0, 0
+clock = pygame.time.Clock()
+        
+FPS = 60
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, y, path):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vx = 5
+        self.vy = 5
+
+    def update(self):
+        if (self.rect.y + self.rect.height >= height) or (self.rect.y <= 0):
+            self.vy *= -1
+        if (self.rect.x + self.rect.width >= width) or (self.rect.x <= 0):
+            self.vx *= -1
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, path):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.rect.x = width // 2 - self.rect.width // 2
+        self.rect.y = height - 50
+        self.vx = 0
+
+    def update(self):
+        if (self.rect.x + self.vx >= 0 and self.rect.x + self.rect.width + self.vx <= width):
+            self.rect.x += self.vx
+
 
 def main():
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode(size)
-    sprite = Sprite(5, 5, 'basketball.png')
-    sprite.render()
-    class Game_Object():
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-    class Ball(Game_Object):
-        def __init__(self, x, y):
-            super().__init__(x, y)
-            self.shift_x = 5
-            self.shift_y = 5
-            self.textured_object = pygame.image.load('basketball.png')
-            self.rect = self.textured_object.get_rect()
-            self.rect.x = x
-            self.rect.y = y
-
-        def logic(self):
-            if (self.rect.y + self.rect.height >= height) or (self.rect.y <= 0):
-                self.shift_y *= -1
-            if (self.rect.x + self.rect.width >= width) or (self.rect.x <= 0):
-                self.shift_x *= -1
-            self.rect.x += self.shift_x
-            self.rect.y += self.shift_y
-        
-        def draw(self):
-            screen.blit(self.textured_object, self.rect)
 
     game_over = False
-    shift_x = 1
-    shift_y = 1
-    current_shift = 0
-    rect_x = 0
-    flag = False
-    ball = Ball(width // 2, height // 2)
+
+    sprites = pygame.sprite.Group()
+
+    ball = Ball(100, 100, 'basketball.png')
+
+    platform = Platform('platform.png')
+
+
+    sprites.add(ball)
+    sprites.add(platform)
 
 
     while not game_over:
+
+        clock.tick(FPS)
+
         # Events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    current_shift = -1
+                    platform.vx = -5
                 elif event.key == pygame.K_d:
-                    current_shift = 1
+                    platform.vx = 5
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_a or event.key == pygame.K_d:
-                    current_shift = 0
+                    platform.vx = 0
         # Logic
-        ball.logic()
+        sprites.update()
+        if pygame.sprite.collide_rect(ball, platform):
+            ball.vy *= -1
         # Draw
         screen.fill(black) 
-        ball.draw()
+        sprites.draw(screen)
         # Draw flip and wait
         pygame.display.flip()
-        pygame.time.wait(10)
+
     sys.exit()
 
 
