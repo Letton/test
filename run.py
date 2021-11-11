4649ab18023c8da29a556fa9d6e211f3e2a81dd4
Letton <admin@letton.tech>
2021-11-11 15:18:01 +0300

    Final

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
-size = width, height = 500, 500
+size = width, height = 700, 600
 black = 0, 0, 0
-clock = pygame.time.Clock()
-        
+clock = pygame.time.Clock()   
+colors = [(94, 189, 62), (255, 185, 0), (247, 130, 0), (226, 56, 56), (151, 57, 153), (0, 156, 223)]
 FPS = 60
 
 def detect_collision(dx, dy, ball, rect):
@@ -25,21 +25,21 @@ def detect_collision(dx, dy, ball, rect):
     return dx, dy
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, x, y, path):
+    def __init__(self, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([10, 10])
-        pygame.draw.circle(self.image, (255, 255, 0), (5, 5), 5)
+        self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.vx = 2
-        self.vy = 2
-
+        self.vx = speed
+        self.vy = speed
+    
     def update(self):
-        if (self.rect.y + self.rect.height >= height) or (self.rect.y <= 0):
-            self.vy *= -1
-        if (self.rect.x + self.rect.width >= width) or (self.rect.x <= 0):
-            self.vx *= -1
+        if self.rect.centerx < self.rect.width or self.rect.centerx > width - self.rect.width:
+            self.vx = -self.vx
+        if self.rect.centery < self.rect.height:
+            self.vy = -self.vy
         self.rect.x += self.vx
         self.rect.y += self.vy
 
@@ -54,112 +54,102 @@ class Platform(pygame.sprite.Sprite):
         self.vx = vx
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, i, j, color):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([100, 50])
-        self.image.fill((0, 0, 255))
+        self.color = color
+        self.image = pygame.Surface([100, 40])
+        self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
+        self.rect.x = i
+        self.rect.y = j
 
 def main():
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode(size)
 
-    active_game = False
     game_over = False
-    text = pygame.font.SysFont('arial', 36).render('Game Over', True, (255, 255, 255))
 
-    #sprites = pygame.sprite.Group()
+    sprites = pygame.sprite.Group()
 
-    #ball = Ball(300, 500)
+    ball = Ball(300, 400, 4)
 
-    platform = Platform(100, 25, 1)
+    platform = Platform(100, 25, 6)
 
+    game_over = False
+    
+    for i in range(7):
+        for j in range(6):
+            block = Block(i * 100, j * 40, colors[j])
+            sprites.add(block)
 
-    #sprites.add(ball)
-    #sprites.add(platform)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-        key = pygame.key.get_pressed()
-        if key[pygame.K_a] and platform.rect.left > 0:
-            platform.rect.x -= platform.vx
-            print(1)
-        if key[pygame.K_d] and platform.rect.right < width:
-            platform.rect.x += platform.vx
-
-        screen.fill(black)
-        screen.blit(platform.image, platform.rect)
+                if event.type == pygame.QUIT:
+                    exit()
+        while not game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_a] and platform.rect.left > 0:
+                platform.rect.x -= platform.vx
+            if key[pygame.K_d] and platform.rect.right < width:
+                platform.rect.x += platform.vx
+            if (ball.rect.bottom > height) or (len(sprites) <= 0):
+                game_over = True
+            if (ball.rect.colliderect(platform.rect)):
+                if (ball.vx > 0):
+                    dx = ball.rect.right - platform.rect.left
+                else:
+                    dx = platform.rect.right - ball.rect.left
+                if (ball.vy > 0):
+                    dy = ball.rect.bottom - platform.rect.top
+                else:
+                    dy = platform.rect.bottom - ball.rect.top
+                if (abs(dx - dy) < 3):
+                    ball.vy = -ball.vy
+                    ball.vx = -ball.vx
+                elif dx > dy:
+                    ball.vy = -ball.vy
+                elif dy > dx:
+                    ball.vx = -ball.vx
+            for sprite in sprites:
+                if (pygame.sprite.collide_rect(ball, sprite)):
+                    if (ball.vx > 0):
+                        dx = ball.rect.right - sprite.rect.left
+                    else:
+                        dx = sprite.rect.right - ball.rect.left
+                    if (ball.vy > 0):
+                        dy = ball.rect.bottom - sprite.rect.top
+                    else:
+                        dy = sprite.rect.bottom - ball.rect.top
+                    if (abs(dx - dy) < 3):
+                        ball.vy = -ball.vy
+                        ball.vx = -ball.vx
+                    elif dx > dy:
+                        ball.vy = -ball.vy
+                    elif dy > dx:
+                        ball.vx = -ball.vx
+                    platform.image.fill(sprite.color)
+                    ball.image.fill(sprite.color)
+                    global FPS
+                    FPS += 1
+                    sprite.kill()
+            
+            ball.update()
+            screen.fill(black)
+            screen.blit(platform.image, platform.rect)
+            screen.blit(ball.image, ball.rect)
+            sprites.draw(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+        if (len(sprites) > 0):
+            text = pygame.font.SysFont('arial', 36).render('Game Over', True, (255, 255, 255))
+        else:
+            text = pygame.font.SysFont('arial', 36).render('You Win!', True, (255, 255, 255))
+        screen.blit(text, (width//2 - text.get_width()//2, height//2 - text.get_height()//2))
         pygame.display.flip()
-
-
-    # for i in range(7):
-    #     for j in range(5):
-    #         block = Block(i * 100 + 17 * i, j * 50 + 10 * j)
-    #         sprites.add(block)
-
-    # while not active_game:
-        
-    #     for event in pygame.event.get():
-    #             if event.type == pygame.QUIT:
-    #                 active_game = True
-
-    #     while not game_over:
-
-
-    #         clock.tick(FPS)
-
-    #         # Events
-    #         for event in pygame.event.get():
-    #             if event.type == pygame.QUIT:
-    #                 active_game = True
-    #             elif event.type == pygame.KEYDOWN:
-    #                 if event.key == pygame.K_a:
-    #                     platform.shift_x = 0
-    #                     platform.vx = -2.5
-    #                 elif event.key == pygame.K_d:
-    #                     platform.shift_x = 0
-    #                     platform.vx = 2.5
-    #             elif event.type == pygame.KEYUP:
-    #                 if event.key == pygame.K_a:
-    #                     platform.vx = 0
-    #                     platform.shift_x = -2.5
-    #                 if event.key == pygame.K_d:
-    #                     platform.vx = 0
-    #                     platform.shift_x = 2.5
-                    
-    #         # Logic
-    #         sprites.update()
-    #         if pygame.sprite.collide_rect(ball, platform):
-    #             ball.vy *= -1
-    #         for sprite in sprites:
-    #             if (sprite.__class__.__name__ == 'Block'):
-    #                 if (pygame.sprite.collide_rect(ball, sprite)):
-    #                     detect_collision(ball.vx, ball.vy, ball, sprite.rect)
-    #                     sprite.kill()
-    #         if (ball.rect.y + ball.rect.height >= height):
-    #             game_over = True
-    #         if platform.shift_x > 0:
-    #             platform.shift_x -= 0.01
-    #         if platform.shift_x < 0:
-    #             platform.shift_x += 0.01
-    #         # Draw
-    #         screen.fill(black) 
-    #         sprites.draw(screen)
-    #         # Draw flip and wait
-    #         pygame.display.flip()
-    #     for sprite in sprites:
-    #         sprite.kill()
-    #     screen.fill(black) 
-    #     screen.blit(text, (width//2 - 100, height//2 - 30))
-    #     pygame.display.flip()
-
-    # sys.exit()
-
 
 if __name__ == '__main__':
     main()
